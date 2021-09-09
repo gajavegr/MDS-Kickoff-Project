a9350c75f48ac78146eb1f57b79d538fb47b2aea
gajavegr <gajavegr@rose-hulman.edu>
2021-09-09 16:54:39 -0400

    real email this time with subject

--- QuickEmailScript.py
+++ QuickEmailScript.py
@@ -1,16 +1,23 @@
 import ssl,smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 port = 465
 sender_email = "c7z062022@gmail.com"
 password = "Sarerem@22"
 receiver_email = ["gajavegr@rose-hulman.edu","hartle@rose-hulman.edu","davignwh@rose-hulman.edu","hallmr@rose-hulman.edu"]
-message = """\
-Subject: Trying Out Python Writing Emails
+message = MIMEMultipart('This is test mail')
+message['Subject'] = 'Test mail'
+message['From'] = "c7z062022@gmail.com"
+message['To'] = "gajavegr@rose-hulman.edu"
+text = """\
 
+Hello, new email format with an actual subject."""
+part1 = MIMEText(text, "plain")
+message.attach(part1)
 
-Hello team K4! This is Ganesh."""
 context = ssl.create_default_context()
 server = smtplib.SMTP_SSL("smtp.gmail.com", port,context=context)
 server.login(sender_email, password)
-server.sendmail(sender_email, receiver_email, message)
+server.sendmail(sender_email, receiver_email, message.as_string())
 server.quit()
--- buttonServo.py
+++ buttonServo.py
@@ -1,6 +1,9 @@
 import time
 import wiringpi
 import RPi.GPIO as GPIO
+import ssl,smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 # use 'GPIO naming'
 wiringpi.wiringPiSetupGpio()
@@ -32,17 +35,37 @@ try:
     lastButtonState = currentButtonState;
     currentButtonState = GPIO.input(10);
     if lastButtonState == GPIO.HIGH & currentButtonState == GPIO.LOW:
-        print("Button was pushed!")
-        if (angle == 0):
-          for pulse in range(50, 250, 1):
-            wiringpi.pwmWrite(18, pulse)
-            time.sleep(delay_period)
-          angle = 160
-        if (angle == 160):
-          for pulse in range(250, 50, -1):
-            wiringpi.pwmWrite(18, pulse)
-            time.sleep(delay_period)
-          angle = 0
+      print("Button was pushed!")
+      if (angle == 0):
+        for pulse in range(50, 250, 1):
+          wiringpi.pwmWrite(18, pulse)
+          time.sleep(delay_period)
+        angle = 160
+        port = 465
+        sender_email = "c7z062022@gmail.com"
+        password = "Sarerem@22"
+        receiver_email = ["gajavegr@rose-hulman.edu","hartle@rose-hulman.edu","davignwh@rose-hulman.edu","hallmr@rose-hulman.edu"]
+        message = MIMEMultipart('This is test mail')
+        message['Subject'] = 'Patient has taken a pill'
+        message['From'] = "c7z062022@gmail.com"
+        message['To'] = "gajavegr@rose-hulman.edu"
+        text = """\
+
+        Your patient has taken a pill."""
+        part1 = MIMEText(text, "plain")
+        message.attach(part1)
+
+        context = ssl.create_default_context()
+        server = smtplib.SMTP_SSL("smtp.gmail.com", port,context=context)
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+        server.quit()
+        print("email has been sent!")
+      if (angle == 160):
+        for pulse in range(250, 50, -1):
+          wiringpi.pwmWrite(18, pulse)
+          time.sleep(delay_period)
+        angle = 0
 
 except KeyboardInterrupt:
     GPIO.cleanup()
